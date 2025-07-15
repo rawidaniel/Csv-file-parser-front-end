7f2b21d04d17080361334f8046cace1d2b54f099
rawidaniel <rawidaniel@gmail.com>
2025-07-15 18:08:09 +0300

    feat: protect file uplaod page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Index from "./pages/Index";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/" element={<Index />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Index />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   </BrowserRouter>
 );
